93c26d54c9069efe7c3a9ef922973bf037c94cc2
Samu <castillocisternas1994@gmail.com>
2023-10-17 22:15:03 -0300

    cambios samu

---
+++ index.js
@@ -0,0 +1,15 @@
+var num = 0;
+do{
+var num = parseFloat(prompt("Ingrese un numero"))
+  
+}while(isNaN(num))
+
+function factorial (num) {
+  var total = elementb; 
+  for (i=1; i<=num; i++) {
+    total = total * i; 
+  }
+  return total; 
+}
+
+console.log(factorial(num));
